81a1bebf8b0197272adbceafffcf8b154c118484
Apple <apple@Apples-MacBook-Air.local>
2024-11-22 11:56:41 +0530

    ready

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import { Provider } from 'react-redux';
-import store from './store';
-import App from './App'; // Make sure to import App component
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
